1da994ce542f2731c088606e32cd3e69e472f1bf
Abhishek <alm03060199@gmail.com>
2022-07-29 02:03:49 +0530

    update

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ console.log(numbers)
 const moreNumbers = Array(5,2)
 console.log(moreNumbers)
 
-const moreNumbers = Array(5)// it will give the array length and value is undefind or null
-console.log(moreNumbers)
+const moreNumber = Array(5)// it will give the array of  length and value is undefind or null
+console.log(moreNumber)
 
-const yetMoreNumbers = Array.of 
+const yetMoreNumbers = Array.of(1,2) // it will slower for preformance perspective
+console.log(yetMoreNumbers)
 
