cc20c864c0f2b73cb899bc27b2a94eb09092593d
Abhishek <alm03060199@gmail.com>
2022-07-29 02:24:46 +0530

    update

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-console.log("hello world")
-const numbers = [1,2,3];
-console.log(numbers)
+// console.log("hello world")
+// const numbers = [1,2,3];
+// console.log(numbers)
 
-const moreNumbers = Array(5,2)
-console.log(moreNumbers)
+// const moreNumbers = Array(5,2)
+// console.log(moreNumbers)
 
-const moreNumber = Array(5)// it will give the array of  length and value is undefind or null
-console.log(moreNumber)
+// const moreNumber = Array(5)// it will give the array of  length and value is undefind or null
+// console.log(moreNumber)
 
-const yetMoreNumbers = Array.of(1,2) // it will slower for preformance perspective
-console.log(yetMoreNumbers)
-
+// const yetMoreNumbers = Array.of(1,2) // it will slower for preformance perspective
+// console.log(yetMoreNumbers)
+const moreNumber= Array.from("Hello World"); // it is use to convert a string in  charactor of string ["hello"] to ["h","e","l","l","o"]
+console.log(moreNumber);
