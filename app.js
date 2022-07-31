96c9bc00a4c4a6e998bb9d0df03d5a94a04178f2
Abhishek <alm03060199@gmail.com>
2022-07-31 18:01:44 +0530

    update

--- app.js
+++ app.js
@@ -10,5 +10,26 @@
 
 // const yetMoreNumbers = Array.of(1,2) // it will slower for preformance perspective
 // console.log(yetMoreNumbers)
-const moreNumber= Array.from("Hello World"); // it is use to convert a string in  charactor of string ["hello"] to ["h","e","l","l","o"]
-console.log(moreNumber);
+// const moreNumber= Array.from("Hello World"); // it is use to convert a string in  charactor of string ["hello"] to ["h","e","l","l","o"]
+// console.log(moreNumber);
+
+function convertArray(arr) {
+
+    const copy = [];
+    console.log(copy)
+
+    arr.forEach( i => {
+         let n=(i + 1) * arr[i];
+
+         copy.push(n)
+        
+        
+    });
+    console.log(copy)
+
+    return copy
+}
+const arr=[1, 2, 2, 3, 1, 2]
+console.log(convertArray(arr))
+
+
